7022e43ba3ea5fd6b93c72f45fef5fcdbf9b4e43
regNec <39899999+regNec@users.noreply.github.com>
2019-05-25 15:02:23 +0800

    upload file module added

--- book_recommendation/home/views.py
+++ book_recommendation/home/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import UploadFileForm
+import os
+import sys
 
 
 def main(request):
-    context = {}
-    if request.method == 'GET':
-        context['image'] = 'Upload your image'
-    return render(request, 'home/main.html', context)
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            process_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('result.html')
+    else:
+        form = UploadFileForm()
+    return render(request, 'home/main.html', {'form': form})
 
 
 def result(request):
     return render(request, 'home/result.html')
+
+
+def process_uploaded_file(file):
+    # save it first
+    with open(os.path.join(sys.path[0], 'home', 'uploaded_file', file.name), 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
